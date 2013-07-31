5e8dd7213c9474314bfa18e5504866aa6f7cab34
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-07-31 00:26:02 -0700

    added search bar with proper erb format on layout for the time being

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 IflyBy::Application.routes.draw do
+  get 'search' => 'flights#index', :as => "search"
   resources :flights, :only => [:new, :create, :index]
   devise_for :passengers
   root to: "flights#index"
