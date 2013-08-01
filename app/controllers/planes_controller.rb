dacd09a9330a1f261e4a73167a763e5b14910377
eslafroggy <claire.pierce@gmail.com>
2013-07-31 20:10:07 -0700

    added planes controller actions

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -1,13 +1,18 @@
 class PlanesController < ApplicationController
   def new
+    @plane = Plane.new
   end
 
   def create
+    @plane = Plane.new(params[:plane])
+    @plane.save
   end
 
   def show
+    @plane = Plane.find(params[:id])
   end
 
   def index
+    @planes = Plane.all
   end
 end
