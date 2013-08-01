62067b35def7c0e2528f6892a99bdc5c2c52b11d
Amy Erz <wingmerz@gmail.com>
2013-07-31 19:57:48 -0700

    update flights and plane models

--- db/migrate/20130730233351_add_attributes_to_flight_model.rb
+++ db/migrate/20130730233351_add_attributes_to_flight_model.rb
@@ -6,5 +6,10 @@ class AddAttributesToFlightModel < ActiveRecord::Migration
     add_column :flights, :departure_datetime, :datetime
     add_column :flights, :flight_code, :string
     add_column :flights, :airline, :string
+    add_column :flights, :arrival_latitude, :float
+    add_column :flights, :arrival_longitude, :float
+    add_column :flights, :departure_latitude, :float
+    add_column :flights, :departure_longitude, :float
+
   end
 end
