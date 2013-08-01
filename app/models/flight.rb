369e41220e34f3bb4ad53b1844368a25ebbfe46b
Amy Erz <wingmerz@gmail.com>
2013-08-01 08:55:44 -0700

    loop for tickets

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,5 +2,14 @@ class Flight < ActiveRecord::Base
   belongs_to :plane
   attr_accessible :arrival_airport, :arrival_datetime, :departure_datetime, :departure_airport, :flight_code, :airline
 
+before_create make_ticket
+	def make_ticket
+		#create tickets by row, seat & create same flight id      
+    ('A'..'G').each do |current_row|
+      (1..10).each do |current_column|
+          self.id.each do |flight_id|
+          Ticket.create({:row => current_row, :column => current_column, :flight_id => flight.id})
+      end
+    end
 
 end
