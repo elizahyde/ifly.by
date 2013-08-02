1ae0a36f92e069ba22391734b17ede950876ba20
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-08-02 08:50:44 -0700

    added more styling. kinda working branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#seed admin
 Passenger.create({ :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password'})
 
 
-
+#seed airports
 airport_list = [
   ["LAX", "Los Angeles"],
   ["SFO", "San Francisco"],
@@ -23,47 +24,54 @@ airport_list = [
 ]
 
 airport_list.each do |airport_code, city|
-
   Airport.create(airport_code: airport_code,
                  city: city)
-
 end
-# flight_list = [
-# ["LAX", "SFO", "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", "Virgin Airlines" ],
-# ["SFO", "LAX", "2013-07-30 21:30:00", "2013-07-31 00:30:00", "VA070X", "Virgin Airlines" ],
-# ["LAX", "SFO", "2013-08-01 17:30:00", "2013-08-01 20:30:00", "VA069X", "Virgin Airlines" ],
-# ["SFO", "LAX", "2013-08-01 21:30:00", "2013-08-02 00:30:00", "VA070X", "Virgin Airlines" ],
-# ["LAX", "SFO", "2013-08-01 17:30:00", "2013-08-01 20:30:00", "AA001X", "American Airlines" ],
-# ["SFO", "LAX", "2013-08-01 21:30:00", "2013-08-02 00:30:00", "AA002x", "American Airlines" ],
-# ["OAK", "SAC", "2013-08-02 07:00:00", "2013-08-02 08:00:00", "SW023y", "Southwest Airlines" ],
-# ["OAK", "SAC", "2013-08-02 08:00:00", "2013-08-02 09:00:00", "SW024y", "Southwest Airlines" ],
-# ["OAK", "SAC", "2013-09-02 07:00:00", "2013-09-02 08:00:00", "SW025y", "Southwest Airlines" ],
-# ["OAK", "SAC", "2013-09-02 07:00:00", "2013-09-02 08:00:00", "SW025y", "Southwest Airlines" ],
-#
-# ["LAX", "SFO", "2013-09-30 17:30:00", "2013-09-30 20:30:00", "VA069X", "Virgin Airlines" ],
-# ["SFO", "LAX", "2013-09-30 21:30:00", "2013-09-31 00:30:00", "VA070X", "Virgin Airlines" ],
-# ["LAX", "SFO", "2013-10-01 17:30:00", "2013-10-01 20:30:00", "VA069X", "Virgin Airlines" ],
-# ["SFO", "LAX", "2013-10-01 21:30:00", "2013-10-02 00:30:00", "VA070X", "Virgin Airlines" ],
-# ["LAX", "SFO", "2013-11-01 17:30:00", "2013-11-01 20:30:00", "AA001X", "American Airlines" ],
-# ["SFO", "LAX", "2013-11-01 21:30:00", "2013-11-02 00:30:00", "AA002x", "American Airlines" ],
-# ["OAK", "SAC", "2013-11-02 07:00:00", "2013-11-02 08:00:00", "SW023y", "Southwest Airlines" ],
-# ["OAK", "SAC", "2013-12-02 08:00:00", "2013-12-02 09:00:00", "SW024y", "Southwest Airlines" ],
-# ["OAK", "SAC", "2013-12-02 07:00:00", "2013-12-02 08:00:00", "SW025y", "Southwest Airlines" ],
-# ["OAK", "SAC", "2013-12-02 07:00:00", "2013-12-02 08:00:00", "SW025y", "Southwest Airlines" ]
-#
-# ]
-#
-# flight_list.each do |arrival_airport, departure_airport, departure_datetime, arrival_datetime, flight_code, airline|
-# Flight.create( arrival_airport: arrival_airport,
-#   departure_airport: departure_airport,
-#   departure_datetime: departure_datetime,
-#   arrival_datetime: arrival_datetime,
-#   flight_code: flight_code,
-#   airline: airline
-#
-#  )
-#
-# end
+
+#seed planes
+plane_list = [
+  ["B747", 10, 12],
+  ["AB69", 5, 7],
+  ["TTYL", 10, 20],
+  ["BBQ", 3, 8],
+  ["37XY", 6, 10],
+  ["STR4", 3, 9],
+]
+
+plane_list.each do |plane_name, row, column|
+  Plane.create(
+    plane_name: plane_name,
+    row: row,
+    column: column
+    )
+end
+
+#seed flights
+flight_list = [
+[1, 2,  "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", 1 ],
+[2, 3,  "2013-07-30 21:30:00", "2013-07-31 00:30:00", "VA070X", 2 ],
+[3, 4,  "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", 3 ],
+[4, 5,  "2013-07-30 21:30:00", "2013-07-31 00:30:00", "VA070X", 4 ],
+[5, 6,  "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", 5 ],
+[7, 8,  "2013-07-30 21:30:00", "2013-07-31 00:30:00", "VA070X", 6 ],
+[9, 1,  "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", 1 ],
+[1, 2,  "2013-07-30 21:30:00", "2013-07-31 00:30:00", "VA070X", 2 ],
+[2, 3,  "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", 3 ],
+[4, 5,  "2013-08-01 21:30:00", "2013-08-02 00:30:00", "VA070X", 4 ],
+[6, 7,  "2013-07-30 17:30:00", "2013-07-30 20:30:00", "VA069X", 5 ],
+[7, 8,  "2013-08-02 21:30:00", "2013-08-03 00:30:00", "VA070X", 6 ],
+
+]
+
+flight_list.each do |arrival_airport_id, departure_airport_id, departure_datetime, arrival_datetime, flight_code, plane_id|
+  Flight.create( arrival_airport_id: arrival_airport_id,
+    departure_airport_id: departure_airport_id,
+    departure_datetime: departure_datetime,
+    arrival_datetime: arrival_datetime,
+    flight_code: flight_code,
+    plane_id: plane_id
+   )
+end
 #
 #
 # =begin
